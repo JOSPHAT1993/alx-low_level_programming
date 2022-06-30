24823474055e9bf20c4ae936d5028327fa465a29
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-06-30 11:52:25 -0700

    Simple C programme using puts and Betty Styles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main entry
+ * main entr
+ * Start of Main function
+ *
  */
 int main()
 {
+/* Printing the text within the puts function */
 	puts("\"Programming is like building a multilingual puzzle");
-
+/* The program is returning null */
 	return (0);
 }
