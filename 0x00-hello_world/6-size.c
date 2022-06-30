c0354b4d1d42072f19f76ea144f7331547e3c98e
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-06-30 16:40:06 -0700

    Printing sizes of variable types in a comp

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(IntType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(LongintType));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(LlongintType));
-	printf("Size of a float type: %lu byte(s)\n", sizeof(FloatType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(FloatType));
 	return (0);
 }
