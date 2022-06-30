5a30ae34c7917ad0019ac12576808c0f008d997e
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-06-30 15:37:14 -0700

    Printing size of Datatypes in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Description : Printing the sizes of datatypes in Computer
  *
  * return : Always 0 (success)
@@ -10,12 +9,13 @@ int main(void)
 {
 	int IntType;
 	char CharType;
-	double DoubleType;
+	long int LongintType;
+	long long int LlongintType
 	float FloatType;
-
 	printf("The size of Int type : %zu bytes\n", sizeof(IntType));
 	printf("The size of Char type : %zu bytes\n", sizeof(CharType));
-	printf("The size of Double type : %zu bytes\n", sizeof(DoubleType));
+	printf("The size of Long int type : %zu bytes\n", sizeof(LongintType));
+	printf("The size of Long Long int : %zu bytes\n", sizeof(LlongintType));
 	printf("The size of Float type : %zu bytes\n", sizeof(FloatType));
 	return(0);
 }
