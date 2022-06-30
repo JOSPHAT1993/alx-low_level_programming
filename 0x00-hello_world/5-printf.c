444bd1642af739d952bec4e9f878e306519c0247
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-06-30 13:06:33 -0700

    Simple C program using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main : Entry point
- * Description : Printing a simple text
+ * Description : using printf to output a simple text
  * Return : Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
