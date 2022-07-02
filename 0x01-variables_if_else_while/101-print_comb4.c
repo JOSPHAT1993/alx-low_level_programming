d4826fc69301dc469d3bb9027b48faf52bc19a2f
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-02 13:19:04 -0700

    Printing all possible combinations of three integers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description:'Printing all possible combs of three digits'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int p, q, r;
+
+	for (p = 10; p < 20; p++)
+	{
+		for (q = 10; q < 20; q++)
+		{
+			for (r = 10; r < 20; r++)
+			{
+				if (((r % 10) > (q % 10)) && ((q % 10) > (p % 10)))
+				{
+					putchar((p % 10) + '0');
+					putchar((q % 10) + '0');
+					putchar((r % 10) + '0');
+					if ( p != 7 || q != 8 || r != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
