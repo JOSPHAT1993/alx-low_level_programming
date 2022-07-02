1b352143cf981e5b828e84beadadfe088d2bf715
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-02 09:07:36 -0700

    Printing all numbers below 10 and a to f

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int n;
 	char ch;
 
-	for (n = 10; n <=90; n=n+10)
+	for (n = 10; n < 20; n++)
 	{
-		putchar(n % 10, '0');
+		putchar((n % 10) + '0');
 	}
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{
