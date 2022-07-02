59a7ee192b89482dc68ef86f0a695731527b3659
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-02 06:39:19 -0700

    Printing alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description:'Program to print all alphabets except q and e'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char cha;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch = 'e' || ch = 'q')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	return (0);
+}
