f01b04f180baec5b22af03a21b0dc0290a62006f
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-02 13:31:55 -0700

    Printing al possible combinations of three ints less than ten

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 					putchar((p % 10) + '0');
 					putchar((q % 10) + '0');
 					putchar((r % 10) + '0');
-					if (p != 7 || q != 8 || r != 9)
+					if (p != 7 || q != 8)
 					{
 						putchar(',');
 						putchar(' ');
