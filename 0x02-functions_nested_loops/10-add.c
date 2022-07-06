6b0b87d6eac9df4f7dc2bc311f07196305385fb5
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-06 11:41:18 -0700

    Summing two numbers using a function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add-main function
+ *
+ * @c:Integer variable
+ * @d:Integer variable
+ *
+ * Return:sum of c and d
+ */
+int add(int c, int d)
+{
+	int sum = c + d;
+
+	return (sum);
+}
