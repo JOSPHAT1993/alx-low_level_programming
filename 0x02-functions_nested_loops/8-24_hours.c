875846b926e118b590868315a929914fe4af6b80
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-06 09:42:12 -0700

    Printing 24 hrs clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer-main function
+ *
+ * @void:'No value to be declared inside'
+ *
+ * Return:The value of time
+ */
+void jack_bauer(void)
+{
+	int n, j;
+
+	for (n = 0; n < 23; n++)
+	{
+		for (j = 0; j < 59; j++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
