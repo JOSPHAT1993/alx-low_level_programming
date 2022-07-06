e1d3a3b26f04d3ada1bef2b5987d3bb55c147d99
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-06 04:51:56 -0700

    Printng letters a to z ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
  * Description:'Printing a to z 10 times'
  *
  * Return:Returns all the chars
- * */
+ */
 void print_alphabet_x10(void)
 {
 	char ch;
 	int r;
-	
+
 	for (r = 0; r <= 9; r++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
