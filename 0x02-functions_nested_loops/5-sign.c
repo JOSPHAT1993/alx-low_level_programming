000f4b2069437dd9d61b5bcf2b2d6db7fc5f8922
JOSPHAT-MUTUA <josphatmutua502@gmail.com>
2022-07-06 06:15:38 -0700

    Checking for a negative of positive number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign-main function
  *
- * @n:'Relates to the character being checked'
+ * @c:'Relates to the character being checked'
  *
  * Return: 1 if greater than zero, 0 if equal to zero, -1 if less
  */
@@ -11,22 +11,16 @@ int print_sign(int c)
 	if (c > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (c == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else if (c < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 	return (0);
