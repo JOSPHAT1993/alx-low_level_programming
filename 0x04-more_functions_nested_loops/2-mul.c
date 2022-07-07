4d899d30b3dea9b7be07d21615e9b81095bbec43
root <root@kali>
2022-07-07 11:03:19 -0400

    Performing product of numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul-main function for multiplying ints
+ *
+ * @a:'Integer Variable'
+ * @b:'Integer variable'
+ *
+ * Return:Product of integers
+ */
+int mul(int a, int b)
+{
+	int prod = a * b;
+	
+	return (prod);
+}
