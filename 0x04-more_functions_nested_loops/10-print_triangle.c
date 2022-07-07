34ddecd8a267092e4f71690b48acf491d5c789d5
root <root@kali>
2022-07-07 16:47:37 -0400

    Printing a triangle using #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle-main function to print the chars
+ *
+ * @size:'Integer variable to print the character'
+ *
+ * Return:Symbols # in a triangular pattern
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = i; j <= size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+	}
+}
