841a6cba72bb1b16a431d87c355d3b1f4d9d89cc
root <root@kali>
2022-07-07 12:25:31 -0400

    Printing numbers from 0 to 14 using putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,27 +9,19 @@
 void more_numbers(void)
 {
 	int i;
-	int j = 0;
+	int j;
 
-	while ( j <= 9 )
+	for ( j = 0; j <= 9; j++ )
 	{
-		i = 0;
-
-		while (i <= 14)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			{
-				_putchar('1');
-				_putchar((i % 10) + '0');
+				_putchar((i / 10) + '0');
 			}
-			else
-			{
-				_putchar(i % 10 + '0');
-			}
-			i++;	
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
-		j++;
 	}
 
 }
