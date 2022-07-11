0a0b5a6731c54068980e542c057524eb288c8376
root <root@kali>
2022-07-11 05:51:45 -0400

    1-swap.c swaps integer vars in 1-main.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int-main function for swapping values of variables
+ *
+ * @a:'Integer Variable for use'
+ * @b:'Integer Variable used in the function'
+ *
+ * Return:Swapped values of the variable
+ */
+void swap_int(int *a, int *b)
+{ 
+	int p = *a;
+	*a = *b;
+	*b = p;
+}
+	
