060181295eb9d8f79ae066e272b3f0b8789cf948
root <root@kali>
2022-07-11 13:27:00 -0400

    8-print_array.c prints the array values passed in the 8-main.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array-main function for printing elements of an array
+ *
+ * @a:'Array Variable'
+ * @n:'Integer variable representing number of elements'
+ *
+ * Return:Elements in the array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+		printf(", ");
+		}
+	}
+}
