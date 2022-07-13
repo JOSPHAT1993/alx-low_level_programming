ed31bfc1903dd42b32c39e9943b9aa032cc93fdc
root <root@kali>
2022-07-13 10:02:37 -0400

    Changing case of tring to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper-main function entry
+ *
+ * Return:Uppercase letters from lowercase
+ */
+char *string_toupper(char *p)
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+	}
+	return (p);
+}
