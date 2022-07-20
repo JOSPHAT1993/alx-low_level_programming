8e6058087899eded7a87e7528abd52caba7e6641
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-20 07:37:33 -0400

    1-print_rev_recusion.c prints the string in 1-main.c in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion-main function to print a reverse of a string
+ *
+ * @s:'String Variable'
+ *
+ * Return:Reversed string passed to the function
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
