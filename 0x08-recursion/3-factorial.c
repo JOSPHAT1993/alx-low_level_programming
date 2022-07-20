5d52528f3346792699e984132736d2ae1698f460
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-20 08:11:05 -0400

    3-factorial.c prints the factorials of numbers passed to the function

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial-main function to find factorial of a number
+ *
+ * @n:'Integer variable'
+ *
+ * Return:Factorial of a number passed to it
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
