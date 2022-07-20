a200e1b8b81030dbff0d5f78503acc34e296db73
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-20 08:07:42 -0400

    2-strlen_recursion.c prints length of string passed in 2-main.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion-main function returning string len
+ *
+ * @s:'String Variable'
+ *
+ * Return:Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
