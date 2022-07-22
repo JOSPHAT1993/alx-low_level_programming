fefc69e821516fedd80dabc837e4dcb3b89a3a74
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-22 16:24:25 -0400

    Writes the name entered of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main-Entry function
+ *
+ * @argc:'Argument c'
+ * @argv:'Array Variable repre arguments length'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
