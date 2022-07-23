764653ac900c38f62fcd52400a05b2a6dc3dfaeb
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-23 15:09:43 -0400

    Program printing number of arguments passed on commandline

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main-Entry function
+ *
+ * @argc:'Argument length varibale'
+ * @argv:'Array of arguments'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char* argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
