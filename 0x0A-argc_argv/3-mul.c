c3596f5bcbb6173a68aa4868f9611c3851365668
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-23 16:53:45 -0400

    Program multiplies the arguments passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Entry function
+ *
+ * @argc:'Variable for length of args'
+ * @argv:'Variable array'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, a, b;
+	int prod = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argc == 3)
+		{
+			a = atoi(argv[1]);
+			b = atoi(argv[2]);
+			prod = a * b;
+			printf("%d\n", prod);
+			break;
+		}
+		else
+		{
+			printf("Error\n");
+			break;
+		}
+	}
+	return (1);
+}
+
+
