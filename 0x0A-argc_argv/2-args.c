dc566e46806b86fec74bee46dc9f2bf72f40833a
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-23 15:54:42 -0400

    Program printing all the arguments passed to it

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main-Entry function
+ *
+ * @argc:'Variable for number of arguments'
+ * @argv:'Array variable'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
