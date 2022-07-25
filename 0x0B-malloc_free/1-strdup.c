8fc5374579e5c787a0a2c0e86a07fc502bfd6de9
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-07-25 15:50:21 -0400

    1-strdup.c duplicates the string in 1-main.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup-main fuction to duplicate string
+ *
+ * @str:'String variable'
+ *
+ * Return:Pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int c = 0;
+	int i = 0;
+	
+	if (str == NULL)
+		return NULL;
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	dup = (char *) malloc(sizeof(char) * c + 1);
+
+	if (!dup)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < c; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
+}
