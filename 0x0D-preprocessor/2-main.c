0b512b31c7bfc9781410fe0994ce935e745824e3
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-08-01 12:43:11 -0400

    Printing its name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-Entry function for printing the filename
+ *
+ * @void:'Void Variable'
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
