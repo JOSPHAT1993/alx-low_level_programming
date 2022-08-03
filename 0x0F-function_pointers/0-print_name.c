213e40914eca7d74f927e74d9fcc60b67c897345
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-08-03 08:37:24 -0400

    Printing name from function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name-function to print name of passed
+ *
+ * @name:'String variable'
+ * @f:'Function pointer'
+ *
+ * return:Name from main c file
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
