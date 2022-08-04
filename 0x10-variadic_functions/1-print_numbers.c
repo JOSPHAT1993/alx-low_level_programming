1382efd2551c837c9d688866e366f42aeee411ae
JOSPHAT MUTUA <josphatmutuakituku@gmail.com>
2022-08-04 05:49:49 -0400

    Variadic function to print numbers parsed

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers-main function to print numbers
+ *
+ * @seperator:'Const Variable char'
+ * @n:'Variable intenger'
+ *
+ * Return:List of numbers
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list(my_numbers);
+
+	va_start(my_numbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(my_numbers, int));
+		if ( i != n - 1)
+		{
+			printf("%s", seperator);
+		}
+	}
+	va_end(my_numbers);
+}
+
+
