a01d1484a164aa1615f26233fb344b148d55bde7
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-08-15 04:49:18 -0400

    Printing a list of elements in Lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_listint-main function to print a linked list
+ *
+ * @h:'variable pointer to head'
+ *
+ * Return:Linked lists elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	while (h  != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
