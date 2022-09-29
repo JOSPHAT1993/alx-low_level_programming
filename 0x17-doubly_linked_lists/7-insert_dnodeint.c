b6be84bfe3bd329b39d247d9ea1a0ccdec1b14c6
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-29 22:43:29 +0300

    Inserting a node at index n

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,29 +10,45 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr = malloc(sizeof(dlistint_t));
-	dlistint_t *temp;
-	unsigned int i;
+	dlistint_t *tmp, *new;
+	unsigned int dex = 1;
 
-	if (ptr == NULL)
+	tmp = *h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	if ((*h) == NULL)
 	{
+		if (idx == 0)
+		{
+			*h = new;
+			return (new);
+		}
 		return (NULL);
 	}
-	temp = *h;
-	for (i = 0; i < (idx - 1); i++)
+	if (idx == 0)
 	{
-		temp = temp->next;
-		if (temp == NULL)
-		{
-			return (NULL);
-		}
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
 	}
-	ptr->n = n;
-	ptr->prev = temp;
-	ptr->next = temp->next;
-	if (temp->next != NULL)
-		temp->next = ptr->next;
-	temp->next = ptr;
-
-	return (ptr);
+	while (tmp->next != NULL && dex != idx)
+	{
+		tmp = tmp->next;
+		dex++;
+	}
+	if (dex == idx)
+	{
+		new->prev = tmp;
+		new->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = new;
+		tmp->next = new;
+		return (new);
+	}
+	return (NULL);
 }
