dbba2c8e6df2e8a94b5cdb182ad5d30f66d3117f
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-23 19:46:52 +0300

    Create a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,7 +7,7 @@
 #include <string.h>
 
 /**
- * struc hash_node_s-Node of a hash table
+ * struct hash_node_s-Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
